85ff1eb467ce6146889d4bec371174270329ebea
Parveen Kumar <pkjain@gmail.com>
2016-03-24 07:03:05 +0000

    cursor pointer style on button

--- jquery.embedyt.js
+++ jquery.embedyt.js
@@ -1,4 +1,3 @@
-/*global jQuery */
 /*jslint indent: 2 */
 /*!
  * EmbedYt 1.0
@@ -8,7 +7,7 @@
   "use strict";
 
   $.fn.embedyt = function (youid) {
-    var htm = '<div id="player' + youid + '"' + ' style="background-image:url(' + 'http://img.youtube.com/vi/' + youid + '/0.jpg' + ');' + 'width:480px;height:360px;text-align:center;line-height:360px;' +  '">' + '<input type="button" value=" play " />' + '</div>';
+    var htm = '<div id="player' + youid + '"' + ' style="background-image:url(' + 'http://img.youtube.com/vi/' + youid + '/0.jpg' + ');' + 'width:480px;height:360px;text-align:center;line-height:360px;' +  '">' + '<input style="cursor:pointer;" type="button" value=" play " />' + '</div>';
     this.html(htm);
     this.find("div input[type=button]").bind('click', function () {
       var ifhtml = '<iframe width="420" height="315" src="http://www.youtube.com/embed/' + youid + '?autoplay=1" frameborder="0" allowfullscreen></iframe>';
